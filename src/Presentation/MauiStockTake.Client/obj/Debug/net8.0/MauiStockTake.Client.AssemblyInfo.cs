5bbfd89ad8a6d345a0b9390e29f339af0c6461da
Milind104 <patelmilind104@gmail.com>
2024-10-02 08:04:29 +0530

    test2

--- src/Presentation/MauiStockTake.Client/obj/Debug/net8.0/MauiStockTake.Client.AssemblyInfo.cs
+++ src/Presentation/MauiStockTake.Client/obj/Debug/net8.0/MauiStockTake.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiStockTake.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5649001ae34431f332d8a7ab3c87e43f01b1157d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiStockTake.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiStockTake.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
